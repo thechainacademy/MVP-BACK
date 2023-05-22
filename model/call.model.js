7e8e9f7ad2919907dd8687114c09b0177fe72b06
Briofita09 <felipe.osorio.santos@hotmail.com>
2023-05-22 11:36:31 -0300

    fix: userReceiver

--- model/call.model.js
+++ model/call.model.js
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 
 const callSchema = new Schema({
   userCaller: { type: Schema.Types.ObjectId, ref: "User" },
-  userReciver: { type: Schema.Types.ObjectId, ref: "User" },
+  userReceiver: { type: Schema.Types.ObjectId, ref: "User" },
   date: { type: Date, default: Date.now },
   duration: String,
   description: String,
